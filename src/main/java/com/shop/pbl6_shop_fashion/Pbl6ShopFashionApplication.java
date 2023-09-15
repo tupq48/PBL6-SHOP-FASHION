2f069a4ec74142f38ba0cb95e96a77c252bf4688
HQuan <nguyenhoangquan.ph@gmail.com>
2023-09-15 11:11:14 +0700

    init

---
+++ src/main/java/com/shop/pbl6_shop_fashion/Pbl6ShopFashionApplication.java
@@ -0,0 +1,13 @@
+package com.shop.pbl6_shop_fashion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pbl6ShopFashionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pbl6ShopFashionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shop/pbl6_shop_fashion/Pbl6ShopFashionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.pbl6_shop_fashion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pbl6ShopFashionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
