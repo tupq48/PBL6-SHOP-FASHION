b238d747fd23f78679a0e8e397700ad8d8720b85
YukiVt <xuanlong1234321@gmail.com>
2023-12-23 15:52:16 +0700

    service calculate each order item amount

--- src/main/java/com/shop/pbl6_shop_fashion/service/impl/ProductSizeServiceImpl.java
+++ src/main/java/com/shop/pbl6_shop_fashion/service/impl/ProductSizeServiceImpl.java
@@ -19,9 +19,19 @@ public class ProductSizeServiceImpl implements ProductSizeService {
     @Override
     public boolean increaseSoldOut(Product product, Size size, Integer amountSoldOut) {
         ProductSize productSize = productSizeRepository.findByProductAndSize(product, size).get();
-        if (productSize.getQuantity() - productSize.getQuantitySold() < amountSoldOut)
+        if (productSize.getQuantity() < amountSoldOut)
             throw new RuntimeException("Number Product not enough to sell");
         productSize.setQuantitySold(productSize.getQuantitySold() + amountSoldOut);
+        productSize.setQuantity(productSize.getQuantity() - amountSoldOut);
+        productSizeRepository.save(productSize);
+        return true;
+    }
+
+    @Override
+    public boolean rollbackSoldOut(Product product, Size size, Integer amountSoldOut) {
+        ProductSize productSize = productSizeRepository.findByProductAndSize(product, size).get();
+        productSize.setQuantity(productSize.getQuantity() + amountSoldOut);
+        productSize.setQuantitySold(productSize.getQuantitySold() - amountSoldOut);
         productSizeRepository.save(productSize);
         return true;
     }
