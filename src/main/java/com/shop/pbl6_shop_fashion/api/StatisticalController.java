b216343aa9f5608b59be52ed7ba977ff4b0c1de7
Phạm Chính Hiệu <chinhhieu0906@gmail.com>
2023-12-26 10:28:17 +0700

    update code 26/12/2023

--- src/main/java/com/shop/pbl6_shop_fashion/api/StatisticalController.java
+++ src/main/java/com/shop/pbl6_shop_fashion/api/StatisticalController.java
@@ -1,5 +1,6 @@
 package com.shop.pbl6_shop_fashion.api;
 
+import com.shop.pbl6_shop_fashion.dto.RevenueStatistics;
 import com.shop.pbl6_shop_fashion.dto.StatisticalDto;
 import com.shop.pbl6_shop_fashion.service.StatisticalService;
 
@@ -7,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/statistical")
@@ -16,6 +20,10 @@ public class StatisticalController {
     @GetMapping()
     public StatisticalDto generalStatistics(){
         return statisticalService.generalStatistics();
+    }
+    @GetMapping("/revenueStatistics")
+    public List<RevenueStatistics> revenueStatistics() throws ParseException {
+        return statisticalService.revenueStatistics();
     }
 
 }
