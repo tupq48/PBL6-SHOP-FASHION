3d4547ac0b5266c469ced92eaab361885255924b
HQuan <nguyenhoangquan.ph@gmail.com>
2023-12-29 19:04:18 +0700

    add cmt with order

--- src/main/java/com/shop/pbl6_shop_fashion/api/CommentController.java
+++ src/main/java/com/shop/pbl6_shop_fashion/api/CommentController.java
@@ -2,10 +2,8 @@ package com.shop.pbl6_shop_fashion.api;
 
 import com.shop.pbl6_shop_fashion.dto.comment.CommentDto;
 import com.shop.pbl6_shop_fashion.service.CommentService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -17,18 +15,17 @@ public class CommentController {
 
     @PostMapping()
     public void addComment(@RequestParam("rate") Integer rate,
+                           @RequestParam("orderItemId") Integer orderItemId,
                            @RequestParam("productId") Integer productId,
                            @RequestParam("userId") Integer userId,
-                           @RequestParam("content") String content
-    ) {
-        commentService.addComment(productId, rate, userId, content);
+                           @RequestParam("content") String content) {
+        commentService.addComment(productId, rate, userId, content,orderItemId);
     }
 
     @PutMapping("/{commnetId}")
     public void updateProduct(@RequestParam(value = "commentId") Integer commentId,
                               @RequestParam("rate") Integer rate,
-                              @RequestParam("content") String content
-    ) {
+                              @RequestParam("content") String content) {
         commentService.updateComment(commentId, rate, content);
     }
 
--- src/main/java/com/shop/pbl6_shop_fashion/service/CommentService.java
+++ src/main/java/com/shop/pbl6_shop_fashion/service/CommentService.java
@@ -1,43 +1,54 @@
 package com.shop.pbl6_shop_fashion.service;
 
 import com.shop.pbl6_shop_fashion.dao.CommentRepository;
+import com.shop.pbl6_shop_fashion.dao.OrderItemRepository;
 import com.shop.pbl6_shop_fashion.dto.comment.CommentDto;
 import com.shop.pbl6_shop_fashion.entity.*;
+import com.shop.pbl6_shop_fashion.exception.OrderException;
 import jakarta.persistence.EntityManager;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class CommentService {
-    @Autowired
-    private CommentRepository commentRepository;
-    @Autowired
-    EntityManager entityManager;
-    public void addComment(Integer productId, Integer rate, Integer userId, String content) {
+    private final CommentRepository commentRepository;
+    private final EntityManager entityManager;
+    private final OrderItemRepository orderItemRepository;
 
+
+    public void addComment(Integer productId, Integer rate, Integer userId, String content, Integer orderItemId) {
         Comment comment = new Comment();
+        OrderItem orderItem = orderItemRepository.findById(orderItemId)
+                .orElseThrow(() -> new OrderException("Order Item Not Found", HttpStatus.NOT_FOUND));
+        if (orderItem.isRate()) {
+            throw new OrderException("Order Item Was Commented", HttpStatus.BAD_REQUEST);
+        }
         Product product = entityManager.find(Product.class, productId);
         User user = entityManager.find(User.class, userId);
-        System.out.println("User:" + user);
-
         comment.setVisible(true);
         comment.setUser(user);
         comment.setProduct(product);
         comment.setRate(rate);
         comment.setCreateAt(LocalDateTime.now());
         comment.setContent(content);
-
         commentRepository.save(comment);
+
+        orderItem.setRate(true);
+        orderItemRepository.save(orderItem);
     }
 
     public void updateComment(Integer commentId, Integer rate, String content) {
         Comment comment = entityManager.find(Comment.class, commentId);
-        if(comment == null) return;
-        if(rate != null) comment.setRate(rate);
-        if(content !=null &&!content.equals(comment.getContent())){
+        if (comment == null)
+            return;
+        if (rate != null)
+            comment.setRate(rate);
+        if (content != null && !content.equals(comment.getContent())) {
             comment.setContent(content);
             comment.setCreateAt(LocalDateTime.now());
         }
